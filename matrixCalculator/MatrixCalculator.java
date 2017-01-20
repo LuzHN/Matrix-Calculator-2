ec50028284619b3ab1f16c28a11b7745c6e2451d
MauriJHN <maurijhn1@gmail.com>
2017-01-19 20:42:15 -0400

    Update MatrixCalculator.java
    Update on January 19, 2017

--- matrixCalculator/MatrixCalculator.java
+++ matrixCalculator/MatrixCalculator.java
@@ -35,6 +35,15 @@ public class MatrixCalculator extends JPanel implements ActionListener {
 	 * the calculation
 	 */
 	private JSpinner rows, columns, matrixCounter;
+	
+	/** spinner models for each dimension spinner */
+	SpinnerNumberModel rowModel, columnModel;
+
+	/**
+	 * number of matrices to be evaluated, initially 2 since addition is
+	 * selected
+	 */
+	private int matrixNumber = 2;
 
 	/**
 	 * button that creates the window interfaces used to input values into the
@@ -54,23 +63,26 @@ public class MatrixCalculator extends JPanel implements ActionListener {
 	private ArrayList<String> operationNames;
 
 	/** default font for the program */
-	public static Font defaultFont = new Font("Berlin Sans FB", Font.PLAIN, 20);
+	public static final Font DEFAULT_FONT = new Font("Berlin Sans FB", Font.PLAIN, 20);
 
 	/** the list of matrices being operated */
 	private LinkedList<MatrixWindow> matrixWindows;
-	
+
 	/** in charge of distributing the windows of the matrices */
 	private WindowDistributor distributor;
-	
-	/**list of matrices that result after all values have been entered*/
+
+	/** list of matrices that result after all values have been entered */
 	private LinkedList<Matrix> rMatrices;
-	
-	/**object in charge of finding the solution of the calculation*/
+
+	/** object in charge of finding the solution of the calculation */
 	private Solution solution;
-	
+
 	/** the operation type selected */
 	private Operation operation = Operation.ADDITION;
-	
+
+	/** determines whether an operation can only be done on a single matrix */
+	private boolean single = false;
+
 	public MatrixCalculator() {
 
 		// set the grid bag layout for the panel
@@ -81,8 +93,10 @@ public class MatrixCalculator extends JPanel implements ActionListener {
 		columnsLabel = new JLabel("Columns");
 		matrixCount = new JLabel("Number of Matrices");
 		operationLabel = new JLabel("Operation");
-		rows = new JSpinner(new SpinnerNumberModel(2, 2, 20, 1));
-		columns = new JSpinner(new SpinnerNumberModel(2, 2, 20, 1));
+		rowModel = new SpinnerNumberModel(2, 2, 20, 1);
+		columnModel = new SpinnerNumberModel(2, 2, 20, 1);
+		rows = new JSpinner(rowModel);
+		columns = new JSpinner(columnModel);
 		matrixCounter = new JSpinner(new SpinnerNumberModel(2, 2, 10, 1));
 		createMatrices = new JButton("Enter");
 		createMatrices.addActionListener(this);
@@ -93,8 +107,10 @@ public class MatrixCalculator extends JPanel implements ActionListener {
 		operationNames.add("Subtraction");
 		operationNames.add("Multiplication");
 		operationNames.add("Inverse");
+		operationNames.add("Transpose");
 		String[] tempArray = new String[operationNames.size()];
 		operations = new JComboBox<String>(operationNames.toArray(tempArray));
+		operations.addActionListener(this);
 
 		// add all components to this panel
 		Window.addComponent(this, rowsLabel, 0, 0, 1, 1, Window.none, Window.center, 5);
@@ -108,7 +124,7 @@ public class MatrixCalculator extends JPanel implements ActionListener {
 		Window.addComponent(this, matrixCounter, 2, 2, 2, 1, Window.hor, Window.center, 5);
 
 		// set the theme of the calculator interface
-		Window.setTheme(this, new Color(0, 40, 0), Color.WHITE, defaultFont);
+		Window.setTheme(this, new Color(0, 40, 0), Color.WHITE, DEFAULT_FONT);
 
 		// create the main frame that will contain the calculator panel
 		window = new Window(this, "Matrix Calculator", Window.EXIT_ON_CLOSE, true);
@@ -127,61 +143,99 @@ public class MatrixCalculator extends JPanel implements ActionListener {
 		Object source = e.getSource();
 
 		if (source == createMatrices) { // the button
-			
-//			declare the value of the matrixWindow list
+
+			// declare the value of the matrixWindow list
 			matrixWindows = new LinkedList<MatrixWindow>();
-			
+
 			// we want to create the number of matrices specified by the spinner
 			// and then store them into the linked list and pass the link
-			
-//			number of rows and columns of the matrices
-			int rowNumber = (int)rows.getValue();
-			int colNumber = (int)columns.getValue();
-			
-//			create the list matrix windows
-			for(int i = 0; i < (int)matrixCounter.getValue(); i++){
-				
-//				the corresponding matrix for the window, we cast int since we know there's a number there
-				Matrix tempMatrix = new Matrix(rowNumber, colNumber);
-				
-				matrixWindows.add(new MatrixWindow(tempMatrix));
-				
+			if(single){
+				matrixNumber = 1;
+			}else{
+				matrixNumber = (int)matrixCounter.getValue();
 			}
 			
-//			FOR TESTING: print the size of the matriWindows list
+			// number of rows and columns of the matrices
+			int rowNumber = (int) rows.getValue();
+			int colNumber = (int) columns.getValue();
+
+			// create the list matrix windows
+			for (int i = 0; i < matrixNumber; i++) {
+
+				// the corresponding matrix for the window
+				Matrix tempMatrix = new Matrix(rowNumber, colNumber);
+
+				matrixWindows.add(new MatrixWindow(tempMatrix));
+
+			}
+
+			// FOR TESTING: print the size of the matriWindows list
 			System.out.println("matrices displayed: " + matrixWindows.size());
-			
-//			create the windowDistributor object since we now have the matrices added to the list
+
+			// create the windowDistributor object since we now have the
+			// matrices added to the list
 			distributor = new WindowDistributor(matrixWindows);
-			
-//			display all windows in the screen
+
+			// display all windows in the screen
 			distributor.displayWindows();
-			
-//			get the operation index
+
+			// create the solution object and pass the list of displayed windows
+			solution = new Solution(matrixWindows, operation);
+
+		} else if (source == operations) {
+
+			// get the operation index
 			int operationIndex = operations.getSelectedIndex();
-			
-//			determine which operation is going to be executed
-			switch(operationIndex){
-			
+
+			// determine which operation is going to be executed
+			switch (operationIndex) {
 			case 0:
 				operation = Operation.ADDITION;
 				System.out.println("Addition selected");
+				// if transpose or inverse was selected
+				single = false;
+//				set the spinner model back to default for row counter
+				rows.setModel(rowModel);
+				// set the matrix counter spinner to visible
+				matrixCounter.setVisible(true);
 				break;
 			case 1:
 				operation = Operation.SUBTRACTION;
 				System.out.println("Subtraction selected");
+				// get the number of matrices to be worked on
+				single = false;
+//				set the spinner model back to default for row counter
+				rows.setModel(rowModel);
+				// set the matrix counter spinner to visible
+				matrixCounter.setVisible(true);
 				break;
 			case 2:
 				operation = Operation.MULTIPLICATION;
 				System.out.println("Multiplication selected");
+				// get the number of matrices to be worked on
+				single = false;
+//				make the spinners point to a single spinner model
+				rows.setModel(columnModel);
+				// set the matrix counter spinner to visible
+				matrixCounter.setVisible(true);
+				break;
 			case 3:
 				operation = Operation.INVERSE;
 				System.out.println("Inverse selected");
+				// inverse only has 1 matrix to work with
+				single = true;
+				// hide the spinner of matrix counter
+				matrixCounter.setVisible(false);
+				break;
+			case 4:
+				operation = Operation.TRANSPOSE;
+				System.out.println("Transpose selected");
+				// transpose only has 1 matrix to work with
+				single = true;
+				// hide the spinner of matrix counter
+				matrixCounter.setVisible(false);
 				break;
 			}
-			
-//			create the solution object and pass the list of displayed windows
-			solution = new Solution(matrixWindows, operation);
 
 		}
 
